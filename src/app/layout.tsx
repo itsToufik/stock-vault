b42141ce32781c76bd3b930e02fb00b9e43a7b28
itsToufik <toutougameryt@gmail.com>
2026-09-17 22:22:38 +0100

    Set up theme management with the "next-themes" library.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,25 +2,31 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "next-themes";
+
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "StockVault",
-  description: "Inventory and Order management dashboard.",
+    title: "StockVault",
+    description: "Inventory and Order management dashboard.",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
-  return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+            <body>
+                <ThemeProvider attribute="class">
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import styles from "./page.module.css";
+"use client";
+
+import "./globals.css";
+
+import { useTheme } from "next-themes";
 
 export default function Home() {
+    const { theme, setTheme } = useTheme();
+    
     return (
-        <div className={styles.page}>
-            
+        <div>
+            <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+                Toggle Theme
+            </button>
         </div>
     );
 }
